a134d31c69566e6a4828d5e4475f979ad4a4afee
unknown <r.mathieu@J001-0140-15.etudiant.rubika-edu.com.local>
2016-04-28 13:00:30 +0200

    Ajout standard asset 1st perso et début interactive + shoot

---
+++ Assets/Scripts/Player/Gun/Shoot.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+    //Linked game objects
+    GameObject currentGun;
+
+    // Gun values
+    public int currentGunAmmo;
+    public int currentGunMaxClip = 6;
+    public int ammoStored;
+
+    // timers
+    public float reloadTime = 0.1f;
+    public float shootDelay = 0.01f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        if (currentGun == null)
+        {
+            currentGun = GameObject.FindGameObjectWithTag("Arm");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void Reload()
+    {
+        if (currentGunAmmo < currentGunMaxClip)
+        {
+            ammoStored -= currentGunMaxClip - currentGunAmmo;
+            currentGunAmmo = currentGunMaxClip;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Interactive/Interact.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Interact : MonoBehaviour {
+
+    public List<Interactible> interactibles;
+
+	// Use this for initialization
+	void Start () {
+        interactibles = new List<Interactible>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetButtonDown("Interact"))
+        {
+            for (int i = 0; i < interactibles.Count; i++)
+            {
+                interactibles[i].Use();
+            }
+        }
+	}
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if(coll.gameObject.CompareTag("Interactible") && !interactibles.Contains(coll.GetComponent<Interactible>()))
+        {
+            interactibles.Add(coll.GetComponent<Interactible>());
+        }
+    }
+
+    void OnTriggerExit(Collider coll)
+    {
+        if(coll.gameObject.CompareTag("Interactible") && interactibles.Contains(coll.GetComponent<Interactible>()))
+        {
+            interactibles.Remove(coll.GetComponent<Interactible>());
+        }
+    }
+}
